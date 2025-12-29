7c7820cf51c4aa8926f4580cfdc9c8a7a2d3c303
Zachary <zachary@tamas.sh>
2025-12-29 13:07:18 -0500

    Add .config/nvim/cspell.config.yaml Update .config/nvim/lazy-lock.json Update .config/nvim/lua/base/init.lua Add .config/nvim/lua/base/lazy/catppuccin.lua Add .config/nvim/lua/base/lazy/neotree.lua Add .config/nvim/lua/base/lazy/treesitter.lua

--- src/dot_config/nvim/lua/base/init.lua
+++ src/dot_config/nvim/lua/base/init.lua
@@ -2,13 +2,22 @@ vim.g.mapleader = " " -- Set leader key before Lazy
  
 require("base.init_lazy")
 
-vim.opt.nu = true                 -- set line numbers -- set line numbers
-vim.opt.relativenumber = true     -- use relative line numbers
+vim.cmd("set nu")                 -- set line numbers -- set line numbers
+vim.cmd("set relativenumber")     -- use relative line numbers
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.smartindent = true
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set expandtab")
+vim.cmd("set smartindent")
+vim.cmd("set termguicolors")
 
-vim.opt.termguicolors = true
+vim.cmd("colorscheme catppuccin")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+vim.keymap.set('n', '<C-b>', ':Neotree filesystem toggle reveal left<CR>')
---
+++ src/dot_config/nvim/lua/base/lazy/neotree.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-tree/nvim-web-devicons",
+    },
+    lazy = false,
+    ---@module 'neo-tree'
+    ---@type neotree.Config
+    opts = {
+      -- close_if_last_window = true,
+      filesystem = {
+        follow_current_file = { enabled = true },
+      },
+    },
+  },
+}
